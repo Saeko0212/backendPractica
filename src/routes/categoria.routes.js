583a6eb5dd0d9c848a12f3adbadde4bc6ee22153
Saeko0212 <derlingcruz2@gmail.com>
2025-04-23 17:10:04 -0600

    listo

--- src/routes/categoria.routes.js
+++ src/routes/categoria.routes.js
@@ -1,12 +1,15 @@
 import { Router } from 'express';
-import {  obtenerCategorias, registrarCategoria } from '../controllers/categoria.controller.js';
+import {  obtenerCategorias, registrarCategoria, eliminarCategoria, actualizarCategoria } from '../controllers/categoria.controller.js';
 
 const router = Router();
 
 // Ruta para obtener todos los clientes
 router.get('/categoria', obtenerCategorias);
 router.post('/registrarcategoria', registrarCategoria);
-
+// Ruta para eliminar un cliente por su ID
+router.delete('/eliminarcategoria/:id', eliminarCategoria);
+// Ruta para actualizar una categoría por su ID
+router.patch('/actualizarcategoria/:id', actualizarCategoria);
 
 
 export default router;
