32af7838d99284cb1c39c8dacc845a330ed9361b
Saeko0212 <derlingcruz2@gmail.com>
2025-05-14 21:40:49 -0600

    listo

--- src/routes/ventas.routes.js
+++ src/routes/ventas.routes.js
@@ -1,9 +1,9 @@
 import { Router } from 'express';
-import { obtenerVentasConDetalles, obtenerVentas, eliminarVenta,registrarVenta } from '../controllers/venta.controller.js';
+import {  obtenerVentasConDetalles, obtenerVentas, eliminarVenta, registrarVenta, actualizarVenta, obtenerVentaPorId } from '../controllers/ventas.controller.js';
 
 const router = Router();
 
-// Ruta para obtener todos los ventas
+// Ruta para obtener todas las ventas
 router.get('/ventas', obtenerVentasConDetalles);
 
 // Ruta para obtener todas las ventas
@@ -15,4 +15,9 @@ router.delete('/eliminarventa/:id_venta', eliminarVenta);
 // Ruta para registrar una nueva venta
 router.post('/registrarventa', registrarVenta);
 
+// Ruta para actualizar una venta
+router.patch('/actualizarventa/:id_venta', actualizarVenta);
+
+//Ruta para obtener ventas por id
+router.get('/obtenerventaporid/:id_venta', obtenerVentaPorId);
 export default router;
